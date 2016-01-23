c0a5f34648b8056438b570aa41d76d71d9c87190
Zhentao <shizhentao@gmail.com>
2016-01-23 22:30:50 +0800

    before lecture 1

---
+++ Lecture_1_basic_R/Julia_packages.jl
@@ -0,0 +1,15 @@
+# this script loads some commonly used packages to Julia for the first use
+# Run these lines in Julia's default console, not in any other editor
+
+Pkg.add("Atom")
+Pkg.add("Distributions")
+Pkg.add("IJulia")
+Pkg.add("PyPlot")
+Pkg.add("Gadfly")
+
+
+using Atom
+using Distributions
+using IJulia
+using PyPlot
+using Gadfly
