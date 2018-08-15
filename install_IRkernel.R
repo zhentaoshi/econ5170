3232dd138e3cd7974bb424feead4c27a0a6df03c
Zhentao Shi <shizhentao@gmail.com>
2018-08-16 00:15:31 +0800

    Update install_IRkernel.R
    add a new package "stringi"

--- install_IRkernel.R
+++ install_IRkernel.R
@@ -1,6 +1,8 @@
-# must executed in Rstudio wit an "admin" status
+# must executed in Rstudio with an "admin" status
 # need to copy the entire library to the "C:program files" location
 
-install.packages(c('repr', 'IRdisplay', 'crayon', 'pbdZMQ', 'devtools'))
+# last successful installation: Microsoft Open R 3.5.0, Aug 16, 2018
+
+install.packages(c('repr', 'IRdisplay', 'crayon', 'pbdZMQ', 'devtools', 'sringi'))
 devtools::install_github('IRkernel/IRkernel', force = TRUE)
 IRkernel::installspec() 
