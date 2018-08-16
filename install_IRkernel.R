87b1bbc38a1344629a831a570eb057343a581388
Zhentao Shi <shizhentao@gmail.com>
2018-08-16 11:51:02 +0800

    Update install_IRkernel.R
    solve the problem of library paths

--- install_IRkernel.R
+++ install_IRkernel.R
@@ -6,3 +6,6 @@
 install.packages(c('repr', 'IRdisplay', 'crayon', 'pbdZMQ', 'devtools', 'stringi', 'uuid', 'digest'))
 devtools::install_github('IRkernel/IRkernel', force = TRUE)
 IRkernel::installspec() 
+
+# In case that Ipython reports "there is no package called 'IRkernel'", check in R 
+# `libPaths()` and syncronize the libraries if necessary.
