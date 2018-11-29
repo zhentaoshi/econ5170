0e76884e4880c099cb49253c6c3f805dceaa5027
Zhentao <shizhentao@gmail.com>
2018-11-30 00:46:57 +0800

    improved python code for unit root simulation

--- Python/unit_root_sim.py
+++ Python/unit_root_sim.py
@@ -13,25 +13,50 @@ import numpy as np
 
 
 def AR(a,T):
-    y = np.zeros(T)
+    y = np.zeros((T,1))
     for t in range(1,T):
         y[t] = a * y[t-1] + np.random.randn(1)
     return y
 
 
 #%%
+    
+# without drift in the regression
 T = 100
 a = 1.0
 
 Rep = 1000
+Bhat0 = np.zeros(Rep)
+
+for r in range(Rep):
+    Y = AR(a,T)
+    y = Y[1:(T+1)]
+    x = Y[range(T-1)]
+    bhat = np.linalg.lstsq(x , y, rcond=None)[0]
+    Bhat0[r] = bhat
+
+#%%
+
+# using PyPlot
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.distplot(Bhat0)
+
+
+#%%
+
+# with a drift in the regression
+
 Bhat = np.zeros(Rep)
 
 for r in range(Rep):
     Y = AR(a,T)
-    y = Y[1:(T+1)]; y = np.reshape(y,(-1,1))
-    x = Y[range(T-1)]; x = np.reshape(x,(-1,1))
+    y = Y[1:(T+1)]
+    x = np.column_stack( (np.ones((T-1,1)), Y[range(T-1)] ) )
     bhat = np.linalg.lstsq(x , y, rcond=None)[0]
-    Bhat[r] = bhat
+    Bhat[r] = bhat[1]
 
 #%%
 
