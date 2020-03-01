0788a9888abdcd92852601c735d6116ab0f1cedb
Zhentao Shi <shizhentao@gmail.com>
2020-03-01 09:50:34 +0800

    update packrat

--- .Rprofile
+++ .Rprofile
@@ -1,3 +1,3 @@
-#### -- Packrat Autoloader (version 0.4.9-2) -- ####
+#### -- Packrat Autoloader (version 0.5.0-25) -- ####
 source("packrat/init.R")
 #### -- End Packrat Autoloader -- ####
--- packrat/init.R
+++ packrat/init.R
@@ -186,7 +186,7 @@ local({
     ## an 'installed from source' version
 
     ## -- InstallAgent -- ##
-    installAgent <- "InstallAgent: packrat 0.4.9-2"
+    installAgent <- "InstallAgent: packrat 0.5.0-25"
 
     ## -- InstallSource -- ##
     installSource <- "InstallSource: source"
