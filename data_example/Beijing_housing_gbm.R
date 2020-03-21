d72613b92275b9a6d4efea1c023bbf1174b8ca93
Zhentao <shizhentao@gmail.com>
2020-03-21 22:35:56 +0800

    smal change to update for parallel computing

--- data_example/Beijing_housing_gbm.R
+++ data_example/Beijing_housing_gbm.R
@@ -1,58 +1,61 @@
+# a new version based on Wang Yishu's implementation
+# As in the paper in Feb 2020
+# Only use 10% of the full data for demonstration
+
+# The full data runs for 8 hours with 24 cores on Econsuper
+
+
 library(caret)
 library(doParallel)
 
+set.seed(666)
+
+# load("data_example/lianjia.RData") # the raw data
 load("lianjia.RData")
+N <- nrow(lianjia) # a smaller sample
+lianjia <- lianjia[base::sample(1:N, round(N / 10)), ]
 
-price_reg=price~
-  t_trade+age+fiveYearsProperty+subway+factor(district)+dist_center+
-  square+livingRoom+drawingRoom+kitchen+bathRoom+
-  factor(floor_type)+floor_total+elevator+ladderRatio+
-  factor(renovationCondition)+factor(buildingType)+factor(buildingStructure)+
-  communityAverage+DOM+followers
+tune_ind <- createDataPartition(1:nrow(lianjia), p = 0.1)$Resample1
+
+gbmGrid <- expand.grid(
+  interaction.depth = seq(from = 10, to = 50, by = 20),
+  n.trees = seq(from = 1000, to = 10000, by = 5000),
+  shrinkage = c(0.001, 0.005, 0.01),
+  n.minobsinnode = 20
+)
+
+gbmControl <- trainControl(method = "cv", number = 5)
 
 
-# GBM
+# With Coordinates
 
-## Tuning Parameters
+formula.GBM <- price ~
+  square + livingRoom + drawingRoom + kitchen + bathRoom +
+  floor_type + floor_total + elevator + ladderRatio +
+  renovationCondition + buildingType + buildingStructure +
+  age + DOM + followers + fiveYearsProperty +
+  subway + district + Lng + Lat + t_trade +
+  communityAverage
 
-train_tune=createDataPartition(1:nrow(lianjia),p=0.1)$Resample1
-gbmGrid=expand.grid(interaction.depth=10:14,
-                    n.trees=(15:20)*100,
-                    shrinkage=c(0.01,0.05,0.1),
-                    n.minobsinnode=20)
-gbmControl=trainControl(method="repeatedcv",number=5,repeats=1)
+tc <- Sys.time()
 
-registerDoParallel(8)
-t=Sys.time()
-boostingReg=train(price_reg,data=lianjia[train_tune,],
-                  method="gbm",distribution="gaussian",
-                  trControl=gbmControl,tuneGrid=gbmGrid,metric="Rsquared",
-                  verbose=F)
-cat("Time Cost of Finding Best Tuning Parameters:",Sys.time()-t,"\n")
+####### run the code
+cat(paste("Starting Tuning with Coordinates at:", tc), "\n")
+registerDoParallel(24)
+Tune.GBM <- train(formula.GBM,
+                  data = lianjia[tune_ind, ],
+                  method = "gbm", distribution = "gaussian",
+                  trControl = gbmControl, tuneGrid = gbmGrid, metric = "Rsquared",
+                  verbose = F
+)
+
 stopImplicitCluster()
+tc <- Sys.time() - tc
+print(tc)
 
-gbmTune=boostingReg$bestTune
-cat("The best tuning parameters for GBM are: \n");print(gbmTune)
-
-## Estimation and Prediction
-
-train_ind=createDataPartition(1:nrow(lianjia),p=0.75)$Resample1
-boostingReg=train(price_reg,data=lianjia[train_ind,],method="gbm",
-                  distribution="gaussian", # to decide the lost function
-                  tuneGrid=gbmTune,verbose=F)
-pred.boosting=predict(boostingReg,newdata=lianjia[-train_ind,])
+gbmTune <- Tune.GBM$bestTune
+print(gbmTune)
+print(Tune.GBM$resample)
 
 
-# LM
-
-lmReg=lm(price_reg,data=lianjia[train_ind,])
-pred.lm=predict(lmReg,newdata=lianjia[-train_ind,])
-
-
-# Comparison
-
-target=lianjia[-train_ind,]$price
-cat("R-squared of GBM prediction =",miscTools::rSquared(target,target-pred.boosting),"\n")
-cat("R-squared of LM prediction =",miscTools::rSquared(target,target-pred.lm),"\n")
-
-
+save(formula.GBM, gbmTune, file = "GBMTune.RData")
