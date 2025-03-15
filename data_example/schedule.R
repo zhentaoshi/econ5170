55b8dbd7ae3cabb707687bd84b803e0d4f5bd1ee
Zhentao <shizhentao@gmail.com>
2025-03-15 11:42:04 +0800

    add notes

--- data_example/schedule.R
+++ data_example/schedule.R
@@ -1,16 +1,59 @@
+# a new version based on Wang Yishu's implementation
+# As in the paper in Feb 2020
+# Only use 15% of the full data for demonstration
 
-quantmod::getSymbols("AAPL",  src = "yahoo") # from = "2017-01-01", to = Sys.Date(),
+# The full data runs for 8 hours with 24 cores on Econsuper
 
-print( tail(AAPL) )
 
-N_AAPL = nrow(AAPL)
-AAPL100 = AAPL$AAPL.Close[(N_AAPL - 100): N_AAPL  ]
 
-quantmod::getSymbols("^GSPC",  src = "yahoo") # from = "2017-01-01", to = Sys.Date(),
+set.seed(666)
 
-print( tail(GSPC))
-N_GSPC = nrow(GSPC)
-GSPC100 = GSPC$GSPC.Close[(N_GSPC - 100): N_GSPC  ]
+# load("data_example/lianjia.RData") # the raw data
+load("lianjia.RData")
+N <- nrow(lianjia) # a smaller sample
+lianjia <- lianjia[base::sample(1:N, round(N * 0.15 )), ]
 
-reg = lm( AAPL100 ~ GSPC100)
-print( coef(reg) )
+tune_ind <- caret::createDataPartition(1:nrow(lianjia), p = 0.1)$Resample1
+
+gbmGrid <- expand.grid(
+  interaction.depth = seq(from = 10, to = 50, by = 10),
+  n.trees = seq(from = 1000, to = 10000, by = 2000),
+  shrinkage = c(0.001, 0.005, 0.01, 0.05),
+  n.minobsinnode = 20
+)
+
+gbmControl <- caret::trainControl(method = "cv", number = 5)
+
+
+# With Coordinates
+
+formula.GBM <- price ~
+  square + livingRoom + drawingRoom + kitchen + bathRoom +
+  floor_type + floor_total + elevator + ladderRatio +
+  renovationCondition + buildingType + buildingStructure +
+  age + DOM + followers + fiveYearsProperty +
+  subway + district + Lng + Lat + t_trade +
+  communityAverage
+
+tc <- Sys.time()
+
+####### run the code
+cat(paste("Starting Tuning with Coordinates at:", tc), "\n")
+doParallel::registerDoParallel(24)
+Tune.GBM <- caret::train(formula.GBM,
+                  data = lianjia[tune_ind, ],
+                  method = "gbm", distribution = "gaussian",
+                  trControl = gbmControl, tuneGrid = gbmGrid, metric = "Rsquared",
+                  verbose = F
+)
+
+doParallel::stopImplicitCluster()
+tc <- Sys.time() - tc
+print(tc)
+
+gbmTune <- Tune.GBM$bestTune
+print(gbmTune)
+print(Tune.GBM$resample)
+
+
+save(formula.GBM, gbmTune, file = "GBMTune.RData")
